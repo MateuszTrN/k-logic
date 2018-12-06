3f67ac75fe2a8a73f42021133824794375e6af61
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-12-06 15:23:37 +0100

    feat(handleAsyncs): modern api with modelDef

--- examples/basic/components/app.js
+++ examples/basic/components/app.js
@@ -1,36 +1,37 @@
 import React, {createElement} from 'react';
+import PropTypes from 'prop-types';
 import {
-    add,
-    addIndex,
-    assoc,
-    composeP,
-    curry,
-    identity,
-    lensPath,
-    lensProp,
-    map,
-    over,
-    pick,
-    set,
-    times,
+  add,
+  addIndex,
+  assoc,
+  composeP,
+  curry,
+  identity,
+  lensPath,
+  lensProp,
+  map,
+  over,
+  pick,
+  set,
+  times,
 } from 'ramda';
 import {actionType, actionType2, createReducer} from 'k-reducer';
 import {compose, withHandlers} from 'recompose';
 import {call, fork, put, takeEvery} from 'redux-saga/effects';
-import {withLogic} from '../../../src/main';
+import {withLogic, handleAsyncs} from '../../../src/main';
 
 const mapWithKey = addIndex(map);
 
 const asyncActionTypeName = curry(
-    (stage, baseType) => `Async/${baseType}/${stage}`
+  (stage, baseType) => `Async/${baseType}/${stage}`
 );
 const succeedAsyncActionName = asyncActionTypeName('Succeeded');
 const failedAsyncActionName = asyncActionTypeName('Failed');
 const requestedAsyncActionName = asyncActionTypeName('Request');
 
 const createAsyncAction = stage => (baseType, payload) => ({
-    type: asyncActionTypeName(stage, baseType),
-    payload,
+  type: asyncActionTypeName(stage, baseType),
+  payload,
 });
 
 const requestAction = createAsyncAction('Request');
@@ -40,141 +41,115 @@ const failedAction = createAsyncAction('Failed');
 const asyncActionRegexp = new RegExp(`^Async/(.+)/(.+)$`);
 
 function* asyncAction({baseType, fn, args}) {
-    try {
-        yield put(requestAction(baseType));
-        const result = yield call(fn, ...(args || []));
-        yield put(succeededAction(baseType, result));
-        return result;
-    } catch (e) {
-        yield put(failedAction(baseType, e));
-    }
+  try {
+    yield put(requestAction(baseType));
+    const result = yield call(fn, ...(args || []));
+    yield put(succeededAction(baseType, result));
+    return result;
+  } catch (e) {
+    yield put(failedAction(baseType, e));
+  }
 }
 
-const handleAsyncs = ({dataProp} = {dataProp: 'data'}) => (
-    model,
-    {type, payload}
-) => {
-    const match = type.match(asyncActionRegexp);
-    if (match) {
-        const resource = match[1];
-        const stage = match[2];
-        if (stage === 'Request') {
-            return set(lensPath([dataProp, resource, 'pending']), true, model);
-        } else if (stage === 'Succeeded') {
-            const m1 = set(
-                lensPath([dataProp, resource, 'pending']),
-                false,
-                model
-            );
-            return set(lensPath([dataProp, resource, 'result']), payload, m1);
-        } else if (stage === 'Failed') {
-            const m1 = set(
-                lensPath([dataProp, resource, 'pending']),
-                false,
-                model
-            );
-            return set(lensPath([dataProp, resource, 'error']), payload, m1);
-        }
-    }
-
-    return model;
-};
-
 const getGists = () =>
-    fetch('https://api.github.com/gists/public').then(r => r.json(), r => r);
+  fetch('https://api.github.com/gists/public').then(r => r.json(), r => r);
 
 const createSaga = ({start}) =>
-    function*() {
-        yield fork(start);
-    };
+  function*() {
+    yield fork(start);
+  };
 
 const fetchOnEvery = ({actions, resourceKey, fn, argsSelector}) =>
-    function*() {
-        yield takeEvery(actions, function*() {
-            yield* asyncAction({
-                baseType: resourceKey,
-                fn,
-                args: [],
-            });
-        });
-    };
+  function*() {
+    yield takeEvery(actions, function*() {
+      yield* asyncAction({
+        baseType: resourceKey,
+        fn,
+        args: [],
+      });
+    });
+  };
 
 const Scope = withLogic({reducer: () => createReducer({}, [])})(
-    ({children}) => <div>{children}</div>
+  ({children}) => <div>{children}</div>
 );
 
 const Array = withLogic({reducer: () => createReducer({}, [])})(
-    ({of, items, ...rest}) =>
-        mapWithKey(
-            (e, idx) =>
-                createElement(of, {...e, ...rest, key: idx, scope: idx}),
-            items
-        )
+  ({of, items, ...rest}) =>
+    mapWithKey(
+      (e, idx) => createElement(of, {...e, ...rest, key: idx, scope: idx}),
+      items
+    )
 );
 
 const Student = compose(
-    withLogic({
-        reducer: () =>
-            createReducer(
-                {
-                    name: '',
-                    surname: '',
-                    data: {gists: {result: [], pending: false}},
-                },
-                [
-                    actionType('SET_NAME', assoc('name')),
-                    actionType('SET_SURNAME', assoc('surname')),
-                    handleAsyncs(),
-                ]
-            ),
-        saga: createSaga({
-            start: fetchOnEvery({
-                actions: ['SET_NAME'],
-                fn: composeP(
-                    map(pick(['description', 'url'])),
-                    getGists
-                ),
-                resourceKey: 'gists',
-            }),
-        }),
+  withLogic({
+    reducer: () =>
+      createReducer(
+        {
+          name: '',
+          surname: '',
+        },
+        [
+          actionType('SET_NAME', assoc('name')),
+          actionType('SET_SURNAME', assoc('surname')),
+          handleAsyncs({
+            gists: {type: PropTypes.array},
+            dataConnection: {
+              type: PropTypes.object,
+              result: lensProp('connection'),
+            },
+          }),
+        ]
+      ),
+    saga: createSaga({
+      start: fetchOnEvery({
+        actions: ['SET_NAME'],
+        fn: composeP(
+          map(pick(['description', 'url'])),
+          getGists
+        ),
+        resourceKey: 'gists',
+      }),
     }),
-    withHandlers({
-        onNameChange: props => e =>
-            props.dispatch({type: 'SET_NAME', payload: e.target.value}),
-        onSurnameChange: props => e =>
-            props.dispatch({type: 'SET_SURNAME', payload: e.target.value}),
-    })
+  }),
+  withHandlers({
+    onNameChange: props => e =>
+      props.dispatch({type: 'SET_NAME', payload: e.target.value}),
+    onSurnameChange: props => e =>
+      props.dispatch({type: 'SET_SURNAME', payload: e.target.value}),
+  })
 )(({name, onNameChange, surname, onSurnameChange, data}) => (
-    <div>
-        <input value={name} onChange={onNameChange} />
-        <input value={surname} onChange={onSurnameChange} />
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+  <div>
+    <input value={name} onChange={onNameChange} />
+    <input value={surname} onChange={onSurnameChange} />
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  </div>
 ));
 
 const StudentList = compose(
-    withLogic({
-        reducer: () =>
-            createReducer({itemCount: 0}, [
-                actionType2('INC', over(lensProp('itemCount'), add(1))),
-            ]),
-    }),
-    withHandlers({
-        onAddStudentClick: props => e => props.dispatch({type: 'INC'}),
-    })
+  withLogic({
+    reducer: () =>
+      createReducer({itemCount: 0}, [
+        actionType2('INC', over(lensProp('itemCount'), add(1))),
+      ]),
+  }),
+  withHandlers({
+    onAddStudentClick: props => e => props.dispatch({type: 'INC'}),
+  })
 )(({children, itemCount, onAddStudentClick}) => (
-    <div>
-        <Array scope="items" of={Student} items={times(identity, itemCount)} />
-        <button onClick={onAddStudentClick} type="button">
-            Add
-        </button>
-    </div>
+  <div>
+    <Array scope="items" of={Student} items={times(identity, itemCount)} />
+    <button onClick={onAddStudentClick} type="button">
+      Add
+    </button>
+  </div>
 ));
 
 const Projects = () => (
-    <Scope scope="root.dupa">
-        <Student scope="student" />
-    </Scope>
+  <Scope scope="root.dupa">
+    <Student scope="student" />
+  </Scope>
 );
 
 export default Projects;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import React, {Component, createFactory} from 'react';
 import sagaMiddleware from './sagaMiddleware';
 import PropTypes from 'prop-types';
-import {addIndex, assocPath, curry, lensPath, map, set, view} from 'ramda';
+import {
+  addIndex,
+  assocPath,
+  curry,
+  lensPath,
+  map,
+  set,
+  view,
+  lensProp,
+  mapObjIndexed,
+  merge,
+  reduce,
+  keys,
+  compose,
+} from 'ramda';
 import {fromTree, wrapAction} from 'k-reducer';
 import {setDisplayName, wrapDisplayName} from 'recompose';
 import {call, put, takeEvery} from 'redux-saga/effects';
@@ -9,129 +23,118 @@ import {call, put, takeEvery} from 'redux-saga/effects';
 const mapWithKey = addIndex(map);
 
 class ScopedComponent extends Component {
-    constructor(props, context) {
-        super();
-        if (!context.kScope) {
-            this.reducersTree = {};
-        }
+  constructor(props, context) {
+    super();
+    if (!context.kScope) {
+      this.reducersTree = {};
     }
+  }
 
-    static contextTypes = {
-        store: PropTypes.object,
-        kScope: PropTypes.object,
+  static contextTypes = {
+    store: PropTypes.object,
+    kScope: PropTypes.object,
+  };
+
+  static childContextTypes = {
+    kScope: PropTypes.object,
+  };
+
+  assocReducer(path, reducer) {
+    if (this.context.kScope) {
+      return this.context.kScope.assocReducer(path, reducer);
+    } else {
+      this.reducersTree = assocPath([...path, '.'], reducer, this.reducersTree);
+
+      this.context.store.replaceReducer(fromTree(this.reducersTree));
+    }
+  }
+
+  getCurrentScopePart() {
+    return this.props.scope != null ? ('' + this.props.scope).split('.') : [];
+  }
+
+  getCurrentScope(currentPart) {
+    return [
+      ...(this.context.kScope ? this.context.kScope.scope : []),
+      ...(currentPart ? currentPart : this.getCurrentScopePart()),
+    ];
+  }
+
+  getCurrentReducersTree() {
+    return this.context.kScope
+      ? this.context.kScope.reducersTree
+      : this.reducersTree;
+  }
+
+  getChildContext() {
+    return {
+      kScope: {
+        scope: this.getCurrentScope(),
+        reducersTree: this.getCurrentReducersTree(),
+        assocReducer: this.assocReducer.bind(this),
+      },
     };
+  }
 
-    static childContextTypes = {
-        kScope: PropTypes.object,
-    };
+  dispatch = action =>
+    this.context.store.dispatch(wrapAction(action, ...this.getCurrentScope()));
 
-    assocReducer(path, reducer) {
-        if (this.context.kScope) {
-            return this.context.kScope.assocReducer(path, reducer);
-        } else {
-            this.reducersTree = assocPath(
-                [...path, '.'],
-                reducer,
-                this.reducersTree
-            );
+  getScopedState() {
+    return view(
+      lensPath(this.getCurrentScope()),
+      this.context.store.getState()
+    );
+  }
 
-            this.context.store.replaceReducer(fromTree(this.reducersTree));
-        }
-    }
-
-    getCurrentScopePart() {
-        return this.props.scope != null
-            ? ('' + this.props.scope).split('.')
-            : [];
-    }
-
-    getCurrentScope(currentPart) {
-        return [
-            ...(this.context.kScope ? this.context.kScope.scope : []),
-            ...(currentPart ? currentPart : this.getCurrentScopePart()),
-        ];
-    }
-
-    getCurrentReducersTree() {
-        return this.context.kScope
-            ? this.context.kScope.reducersTree
-            : this.reducersTree;
-    }
-
-    getChildContext() {
-        return {
-            kScope: {
-                scope: this.getCurrentScope(),
-                reducersTree: this.getCurrentReducersTree(),
-                assocReducer: this.assocReducer.bind(this),
-            },
-        };
-    }
-
-    dispatch = action =>
-        this.context.store.dispatch(
-            wrapAction(action, ...this.getCurrentScope())
-        );
-
-    getScopedState() {
-        return view(
-            lensPath(this.getCurrentScope()),
-            this.context.store.getState()
-        );
-    }
-
-    render() {
-        return this.props.children;
-    }
+  render() {
+    return this.props.children;
+  }
 }
 
 const withLogic = ({reducer, saga}) => BaseComponent => {
-    const factory = createFactory(BaseComponent);
+  const factory = createFactory(BaseComponent);
 
-    class WithReducer extends ScopedComponent {
-        constructor(props, context) {
-            super(props, context);
-        }
-
-        componentWillMount() {
-            if (reducer) {
-                this.assocReducer(this.getCurrentScope(), reducer(this.props));
-            }
-            if (saga) {
-                this.context.store.runSaga(
-                    this.getCurrentScope().join('.'),
-                    saga
-                );
-            }
-        }
-
-        render() {
-            return factory({
-                ...this.props,
-                dispatch: this.dispatch,
-                ...this.getScopedState(),
-            });
-        }
+  class WithReducer extends ScopedComponent {
+    constructor(props, context) {
+      super(props, context);
     }
 
-    if (process.env.NODE_ENV !== 'production') {
-        return setDisplayName(wrapDisplayName(BaseComponent, 'withReducer'))(
-            WithReducer
-        );
+    componentWillMount() {
+      if (reducer) {
+        this.assocReducer(this.getCurrentScope(), reducer(this.props));
+      }
+      if (saga) {
+        this.context.store.runSaga(this.getCurrentScope().join('.'), saga);
+      }
     }
-    return WithReducer;
+
+    render() {
+      return factory({
+        ...this.props,
+        dispatch: this.dispatch,
+        ...this.getScopedState(),
+      });
+    }
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    return setDisplayName(wrapDisplayName(BaseComponent, 'withReducer'))(
+      WithReducer
+    );
+  }
+  return WithReducer;
 };
 
 const asyncActionTypeName = curry(
-    (stage, baseType) => `Async/${baseType}/${stage}`
+  (stage, baseType) => `Async/${baseType}/${stage}`
 );
 const succeedAsyncActionName = asyncActionTypeName('Succeeded');
 const failedAsyncActionName = asyncActionTypeName('Failed');
 const requestedAsyncActionName = asyncActionTypeName('Request');
 
 const createAsyncAction = stage => (baseType, payload) => ({
-    type: asyncActionTypeName(stage, baseType),
-    payload,
+  type: asyncActionTypeName(stage, baseType),
+  payload,
 });
 
 const requestAction = createAsyncAction('Request');
@@ -141,62 +144,116 @@ const failedAction = createAsyncAction('Failed');
 const asyncActionRegexp = new RegExp(`^Async/(.+)/(.+)$`);
 
 function* asyncAction({baseType, fn, args}) {
-    try {
-        yield put(requestAction(baseType));
-        const result = yield call(fn, ...(args || []));
-        yield put(succeededAction(baseType, result));
-        return result;
-    } catch (e) {
-        yield put(failedAction(baseType, e));
-    }
+  try {
+    yield put(requestAction(baseType));
+    const result = yield call(fn, ...(args || []));
+    yield put(succeededAction(baseType, result));
+    return result;
+  } catch (e) {
+    yield put(failedAction(baseType, e));
+  }
 }
 
-const handleAsyncs = ({dataProp} = {dataProp: 'data'}) => (
-    model,
-    {type, payload}
-) => {
+const getStageLens = (modelDef, resource, stage, dataProp) => {
+  const defaultLens = compose(
+    lensProp(dataProp),
+    lensPath([resource, stage])
+  );
+  return modelDef[resource] && modelDef[resource][stage]
+    ? modelDef[resource][stage]
+    : defaultLens;
+};
+
+const buildModelLenses = (modelDef, options) => {
+  const dataProp = options.dataProp || 'data';
+  return mapObjIndexed(
+    (def, resource) => ({
+      pending: getStageLens(modelDef, resource, 'pending', dataProp),
+      result: getStageLens(modelDef, resource, 'result', dataProp),
+      error: getStageLens(modelDef, resource, 'error', dataProp),
+    }),
+    modelDef
+  );
+};
+
+/*
+const modelDef = {
+  a: {
+    result: lensPath(['form', 'fields']),
+  },
+  b: {},
+  c: {},
+};
+
+let model = {hopla: 'test data'};
+
+model = setValue(modelDef, {}, 'a', 'pending', true, model);
+
+model = setValue(modelDef, {}, 'a', 'result', 'xxx', model);
+
+const modelLenses = buildModelLenses(modelDef, {});
+*/
+
+const initModelField = (fieldLens, target) =>
+  compose(
+    set(fieldLens.result, null),
+    set(fieldLens.pending, false),
+    set(fieldLens.error, null)
+  )(target);
+
+const initModel = (modelDef, modelLenses, target) =>
+  reduce(
+    (a, c) => merge(a, initModelField(modelLenses[c], a)),
+    target,
+    keys(modelDef)
+  );
+
+//initModel(modelDef, modelLenses, {dupa: 1});
+
+const handleAsyncs = (modelDef, options = {}) => {
+  const modelLenses = buildModelLenses(modelDef, options);
+
+  return (model, {type, payload}) => {
+    console.log('handleAsyncs', model, type);
+    if (type === '@@INIT') {
+      return initModel(modelDef, modelLenses, model);
+    }
+
     const match = type.match(asyncActionRegexp);
     if (match) {
-        const resource = match[1];
-        const stage = match[2];
-        if (stage === 'Request') {
-            return set(lensPath([dataProp, resource, 'pending']), true, model);
-        } else if (stage === 'Succeeded') {
-            const m1 = set(
-                lensPath([dataProp, resource, 'pending']),
-                false,
-                model
-            );
-            return set(lensPath([dataProp, resource, 'result']), payload, m1);
-        } else if (stage === 'Failed') {
-            const m1 = set(
-                lensPath([dataProp, resource, 'pending']),
-                false,
-                model
-            );
-            return set(lensPath([dataProp, resource, 'error']), payload, m1);
-        }
+      const resource = match[1];
+      const stage = match[2];
+      if (stage === 'Request') {
+        return set(modelLenses[resource].pending, true, model);
+      } else if (stage === 'Succeeded') {
+        const m1 = set(modelLenses[resource].pending, false, model);
+        return set(modelLenses[resource].result, payload, m1);
+      } else if (stage === 'Failed') {
+        const m1 = set(modelLenses[resource].pending, false, model);
+        return set(modelLenses[resource].error, payload, m1);
+      }
     }
 
     return model;
+  };
 };
 
 const fetchOnEvery = ({actions, resourceKey, fn, argsSelector}) =>
-    function*() {
-        yield takeEvery(actions, function*() {
-            yield* asyncAction({
-                baseType: resourceKey,
-                fn,
-                args: [],
-            });
-        });
-    };
+  function*() {
+    yield takeEvery(actions, function*() {
+      yield* asyncAction({
+        baseType: resourceKey,
+        fn,
+        args: [],
+      });
+    });
+  };
 
 export {
-    withLogic,
-    handleAsyncs,
-    asyncAction,
-    fetchOnEvery,
-    sagaMiddleware,
-    ScopedComponent,
+  withLogic,
+  handleAsyncs,
+  asyncAction,
+  fetchOnEvery,
+  sagaMiddleware,
+  ScopedComponent,
 };
