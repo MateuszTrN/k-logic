578712192b51df501cce7b010dd8604bcad74551
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-12-24 12:56:37 +0100

    refactor(withScope): use Scope component instead of duplicated logic

--- src/withScope.js
+++ src/withScope.js
@@ -1,37 +1,12 @@
 import React, {Component, createFactory} from 'react';
-import {pathOr} from 'ramda';
-import {forwardTo} from 'k-reducer';
-import {KLogicContext} from './kLogicProvider';
+import Scope from './scope';
 
 const withScope = BaseComponent => {
   const factory = createFactory(BaseComponent);
 
   class WithScope extends Component {
-    constructor() {
-      super();
-      this.getNewContext = this.getNewContext.bind(this);
-    }
-
-    static contextType = KLogicContext;
-
-    getNewContext() {
-      const scopeArray = this.props.scope.split('.');
-      return this.props.scope
-        ? {
-            ...this.context,
-            dispatch: forwardTo(this.context.dispatch, ...scopeArray),
-            state: pathOr({}, scopeArray, this.context.state),
-            scope: [...this.context.scope, ...scopeArray],
-          }
-        : this.context;
-    }
-
     render() {
-      return (
-        <KLogicContext.Provider value={this.getNewContext()}>
-          {factory(this.props)}
-        </KLogicContext.Provider>
-      );
+      return <Scope scope={this.props.scope}>{factory(this.props)}</Scope>;
     }
   }
 
